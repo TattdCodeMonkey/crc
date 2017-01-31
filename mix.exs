f10cae387c80be0752a9b9e049e0928a72ec1a9b
Rodney Norris <Rodney.Norris@gmail.com>
2017-01-31 06:38:33 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Crc.Mixfile do
 
   def project do
     [app: :crc,
-     version: "0.5.1",
+     version: "0.5.2",
      elixir: ">= 1.0.0 and < 2.0.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
