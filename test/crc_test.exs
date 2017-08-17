10f8cc6d65f7506cdb2888cf4e9d31b776f5852b
Rodney Norris <rnorris@myemma.com>
2017-08-17 08:31:43 -0500

    Clean-up tests
    - Updated crc-8 test to use second test data module attribute
    - Removed commented tests

--- test/crc_test.exs
+++ test/crc_test.exs
@@ -8,16 +8,12 @@ defmodule CRCTest do
   test "calculate correct CRC-8" do
     assert CRC.crc_8(@test_data_01) == 0x7B
   end
+  
+  test "CRC-8 test data 2" do
+    assert CRC.crc_8(@test_data_02) == 0x90
+  end
 
   test "calculate correct checksum (xor)" do
     assert CRC.checksum_xor(@test_data_01) == 0x31
   end
-
-  # test "calculate correct CRC-16 Sick" do
-  #   assert CRC.crc_16_sick(test_data) == 0x56A6
-  # end
-  #
-  # test "calculate correct CRC-DNP" do
-  #   assert CRC.crc_dnp(test_data) == 0x82EA
-  # end
 end
