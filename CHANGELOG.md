97a3a0627ca608186fbeda44ea02d5f48206c590
Kian-Meng Ang <kianmeng@cpan.org>
2021-05-18 20:51:25 +0800

    Misc doc changes
    Besides other documentation changes, this commit ensures the generated
    HTML doc for HexDocs.pm will become the main reference doc for this
    Elixir library which leverage on latest features of ExDoc.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,27 +1,33 @@
 # Changelog
-## v0.10.1
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## v0.10.1 - 2020-06-12
 - Removed usage of `ERL_INTERFACE_INCLUDE_DIR`, it was removed in OTP 23
 
-## v0.10.0
+## v0.10.0 - 2020-01-31
 ### Added
 - Change build path to use `MIX_APP_PATH` if available. This should improve compilation for multiple nerves targets.
 
-## v0.9.2
+## v0.9.2 - 2019-04-09
 ### Fixes
-- Fixed gcc warning implicit declaration of strnlen on linux 
+- Fixed GCC warning implicit declaration of strnlen on GNU/Linux
 
-## v0.9.1
+## v0.9.1 - 2018-03-09
 ### Fixes
-- Removed files from Windows makefile to fix compiling on Windows.
+- Removed files from Windows Makefile to fix compiling on Windows.
 
-## v0.9.0
+## v0.9.0 - 2018-03-09
 ### Added
 - `CRC.calculate/2` generic calculate function that takes the input as first
   parameter. This is a copy of `CRC.crc/2` but should allow easier transition
   from the legacy model functions ie `CRC.crc_16` etc.
 - Many README and Documentation tweaks / improvements.
 
-## v0.8.3
+## v0.8.3 - 2018-02-09
 ### Added
 - `CRC` module docs
 - Docs for generic `CRC.crc` functions
@@ -31,34 +37,33 @@
 - Moved model specific functions from `CRC` to `CRC.Legacy`
 - Removed redundant tests
 
-## v0.8.2
+## v0.8.2 - 2018-01-17
 ### Fixes
 - Fixed cross-compile settings
 - Removed compile-time reference to NIF
 
-## v0.8.1
+## v0.8.1 - 2018-01-11
 ### Fixes
 - Restored Windows build support #21
 ### Added
 - `CRC.list/0` & `CRC.list/1` functions to get full list of pre-defined models
 
-## v0.8.0
+## v0.8.0 - 2017-12-22
 ### Added
 - Generic CRC calculation NIFs `CRC.crc/2`, `CRC.crc_init/1`, `CRC.crc_update/2`, `CRC.crc_final/1`
 - PYCRC property tests to TravisCI build
-- 
 
-## v0.7.1
+## v0.7.1 - 2017-11-29
 ### Fixes
 - Removed compiled NIF files from release #16
 
-## v0.7.0
+## v0.7.0 - 2017-10-19
 ### Added
 - Timeslice reduction counting to NIFs, allowing NIF functions to play nice with the BEAM Scheduler
 - Erlang support & rebar configs
-- NIF based XOR checksum 
+- NIF based XOR checksum
 
-## v0.6.0
+## v0.6.0 - 2017-08-28
 ### Added
 - CRC-16 Kermit
 - Added TravisCI
--- lib/crc.ex
+++ lib/crc.ex
@@ -1,7 +1,7 @@
 defmodule CRC do
   @moduledoc """
   This module is used to calculate CRC (Cyclic Redundancy Check) values
-  for binary data. It uses NIF functions written in C to interate over
+  for binary data. It uses NIF functions written in C to iterate over
   the given binary calculating the CRC checksum value.
 
   CRC implementations have been tested against these online calculators to
@@ -14,65 +14,76 @@ defmodule CRC do
   @doc """
   Calculate a CRC checksum for the `input` based on the crc `params` given.
 
-  `params` can be an atom for one of the compiled models. See `CRC.list/0` for a full list.
-  Or a Map with paramters to create a model at runtime. The map given should have all of the following keys:
+  `params` can be an atom for one of the compiled models. See `CRC.list/0` for
+  a full list or a Map with parameters to create a model at runtime. The map
+  given should have all of the following keys:
 
   `width` - (unsigned integer) representation for the width of the CRC in bits
   `poly` - (unsigned integer) the polynomial used for the CRC calculation
   `init` - (unsigned integer) The initial value used when starting the calculation
   `refin` - (boolean) if the input value should be reflected. This is used for changing between endian's
   `refout` - (boolean) if the outvalue should be reflected when calculation is completed
-  `xorout` - (unsigned integer) Final xor value used when completing the CRC calculation 
-  
-  Example:
-  ```
-  %{
-    width: 16,
-    poly: 0x1021,
-    init: 0x00,
-    refin: false,
-    refout: false,
-    xorout: 0x00
-  }
-  ```
+  `xorout` - (unsigned integer) Final xor value used when completing the CRC calculation
 
-  You can also extend one of the compiled models at runtime by creating a map with `extend` key set to the model you wish to extend
-  and the keys you wish to override for that model.
-  For example to override the initial value for the `:crc_16_ccitt_false` model to `0x1D0F` you would pass the following Map as params:
-  `%{extend: :crc_16_ccitt_false, init: 0x1D0F}`
+  ## Examples
+
+      %{
+        width: 16,
+        poly: 0x1021,
+        init: 0x00,
+        refin: false,
+        refout: false,
+        xorout: 0x00
+      }
+
+  You can also extend one of the compiled models at runtime by creating a map
+  with `extend` key set to the model you wish to extend and the keys you wish
+  to override for that model.
+
+  For example to override the initial value for the `:crc_16_ccitt_false` model
+  to `0x1D0F` you would pass the following Map as params:
+
+      `%{extend: :crc_16_ccitt_false, init: 0x1D0F}`
 
   You can learn more about CRC calculation here:
-  http://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
+  https://www.sunshine2k.de/articles/coding/crc/understanding_crc.html
   """
   @spec crc(:crc_algorithm.params(), iodata()) :: :crc_algorithm.value()
   defdelegate crc(params, input), to: :crc
 
   @doc """
   Initialize a resource to be used for doing CRC calculations. The returned
-  resource can be used with `crc/2` or `crc_update/2` to calcule CRC checksums.
+  resource can be used with `crc/2` or `crc_update/2` to calculate CRC checksums.
 
-  Resource is created using the same `params` types that are used with `crc/2`.
-  - atom's for compiled models
-  - Map with model values
-  - Map to extend a compiled model.
+  Resource is created using the same `params` types that are used with `crc/2`:
 
-  If used with `crc/2` the returned resource can be re-used multiple times, but using a map or atom for a compiled model
-  will likely be slightly more performant.
-  
-  When using with `crc_update/2` a new resource will be returned with every call that should be used to continue the calculation.
+    - atom's for compiled models
+    - Map with model values
+    - Map to extend a compiled model.
+
+  If used with `crc/2` the returned resource can be re-used multiple times, but
+  using a map or atom for a compiled model will likely be slightly more
+  performant.
+
+  When using with `crc_update/2` a new resource will be returned with every
+  call that should be used to continue the calculation.
   """
   @spec crc_init(:crc_algorithm.params()) :: :crc_algorithm.resource()
   defdelegate crc_init(params), to: :crc
-  
+
   @doc """
-  Begin or continue a multi-part CRC calculation. Takes a `resource` from result of `crc_init/1` or previous `crc_update/2`
-  call, and binary `input`, returns a new `resource` to be used to continue or finalize the CRC calculation. 
+  Begins or continues a multi-part CRC calculation.
+
+  Takes a `resource` from result of `crc_init/1` or previous `crc_update/2`
+  call, and binary `input`, returns a new `resource` to be used to continue or
+  finalize the CRC calculation.
   """
   @spec crc_update(:crc_algorithm.resource(), iodata()) :: :crc_algorithm.resource()
   defdelegate crc_update(resource, input), to: :crc
 
   @doc """
-  Takes a `resource` result from `crc_update/2` and finalizes the multi-part CRC calculation.
+  Takes a `resource` result from `crc_update/2` and finalizes the multi-part
+  CRC calculation.
   """
   @spec crc_final(:crc_algorithm.resource()) :: :crc_algorithm.value()
   defdelegate crc_final(resource), to: :crc
@@ -84,10 +95,11 @@ defmodule CRC do
 
   This function has the parameter order reversed to allow easier use with pipelines.
   allowing code to be written like:
-  
-  ```elixir
-  read_data() |> CRC.calculate(:crc_16) |> do_something()
-  ```
+
+  ## Examples
+
+      read_data() |> CRC.calculate(:crc_16) |> do_something()
+
   """
   @spec calculate(iodata(), :crc_algorithm.params()) :: :crc_algorithm.value()
   def calculate(input, params) do
@@ -95,7 +107,7 @@ defmodule CRC do
   end
 
   @doc """
-  Returns a list of all the compiled CRC models
+  Returns a list of all the compiled CRC models.
   """
   @spec list() :: [{atom, String.t}]
   def list() do
